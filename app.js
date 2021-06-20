2cd4d08380a58b1eb4e0b1e9a61d7d9dbda3aac8
Souvik Basu <souvikbasu@gmail.com>
2021-06-20 01:37:28 -0700

    feat(cors): add cors support

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express");
+var cors = require("cors");
 
 module.exports = function (config) {
   var deployFolder = ".";
   var apiRouter = require(deployFolder + "/routes/api-routes")(config);
 
   var app = express();
+  app.use(cors());
 
   app.use("/api", apiRouter);
   return app;
